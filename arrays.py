8ee762daa58cac163ef70a819e74e25d64ffc6fd
David Henao <daves.hb@hotmail.com>
2025-04-23 07:37:30 -0500

    primer commit

---
+++ arrays.py
@@ -0,0 +1,10 @@
+
+
+ciudades ={"mede":"Medellin", "cli":"Cali", "brq":"Barranquilla" }
+
+print(ciudades)
+print(ciudades["cli"])
+print(ciudades["cli"])
+
+
+
---
+++ main.py
@@ -0,0 +1,31 @@
+print("Hola mundo")
+
+
+name = "david"
+lastName = "henaoBustamante"
+
+#print(lastName)
+# print(lastName[:5])
+
+
+# Comentar
+# CTRL + k
+# CTRL + C
+
+# descomentar
+# CTRL + k
+# CTRL + U
+
+
+
+
+
+
+print(name + "-" + "Nombre")
+
+
+
+
+
+
+
